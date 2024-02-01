4467356636376dbe07b15b50ff48d3242e03377c
Will Gibson <8738245+WillGibson@users.noreply.github.com>
2024-02-01 13:27:14 +0000

    master >> main

--- .copilot/phases/build.sh
+++ .copilot/phases/build.sh
@@ -6,7 +6,7 @@ set -e
 # Add commands below to run as part of the build phase
 buildCommand="/work/cli build"
 
-if [ "${CODEBUILD_WEBHOOK_HEAD_REF}" == "refs/heads/master" ]; then
+if [ "${CODEBUILD_WEBHOOK_HEAD_REF}" == "refs/heads/main" ]; then
     buildCommand="${buildCommand} --publish --send-notifications"
 fi
 
