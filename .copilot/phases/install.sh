47fec962d0087eec539fe43d9420e8b272874e42
Will Gibson <8738245+WillGibson@users.noreply.github.com>
2024-02-01 16:39:16 +0000

    DBTP-369 Run unit tests in CodeBuild (#14)
    Co-authored-by: Samuele Mattiuzzo <1381563+samuele-mattiuzzo@users.noreply.github.com>
    Co-authored-by: Lawrence <34475808+acodeninja@users.noreply.github.com>

---
+++ .copilot/phases/install.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# Exit early if something goes wrong
+set -e
+
+# Add commands below to run as part of the install phase
+python --version
+pip install poetry
+poetry install --no-root
---
+++ .copilot/phases/pre_build.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# Exit early if something goes wrong
+set -e
+
+# Add commands below to run as part of the pre_build phase
+poetry run ./tests.sh
