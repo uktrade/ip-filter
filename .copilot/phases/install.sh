554d39c95e1cf35fc9b437c846444c64d14ffd86
Will Gibson <8738245+WillGibson@users.noreply.github.com>
2024-02-02 11:27:51 +0000

    Put brackets round the command

--- .copilot/phases/install.sh
+++ .copilot/phases/install.sh
@@ -8,7 +8,7 @@ set -e
 
 buildCommand="/work/cli build"
 
-if [ "${git rev-parse --abbrev-ref HEAD}" == "DBTP-369-run-unit-tests-in-codebuild-pt3" ]; then
+if [ "$(git rev-parse --abbrev-ref HEAD)" == "DBTP-369-run-unit-tests-in-codebuild-pt3" ]; then
     buildCommand="${buildCommand} --publish --send-notifications"
 fi
 
