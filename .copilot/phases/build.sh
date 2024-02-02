6e85fe26db61c16d2928b387fe75b911c32609db
Will Gibson <8738245+WillGibson@users.noreply.github.com>
2024-02-02 11:52:22 +0000

    Put the new git branch finding stuff in the right place

--- .copilot/phases/build.sh
+++ .copilot/phases/build.sh
@@ -4,9 +4,11 @@
 set -e
 
 # Add commands below to run as part of the build phase
-buildCommand="/work/cli build"
+CODEBUILD_GIT_BRANCH=`git branch -a --contains HEAD | sed -n 2p | awk '{ printf $1 }'`
+CODEBUILD_GIT_BRANCH=${CODEBUILD_GIT_BRANCH#remotes/origin/}
 
-if [ "${CODEBUILD_WEBHOOK_HEAD_REF}" == "refs/heads/main" ]; then
+buildCommand="/work/cli build"
+if [ "${CODEBUILD_GIT_BRANCH}" == "main" ]; then
     buildCommand="${buildCommand} --publish --send-notifications"
 fi
 
--- .copilot/phases/install.sh
+++ .copilot/phases/install.sh
@@ -4,17 +4,6 @@
 set -e
 
 # Add commands below to run as part of the install phase
-CODEBUILD_GIT_BRANCH=`git branch -a --contains HEAD | sed -n 2p | awk '{ printf $1 }'`
-CODEBUILD_GIT_BRANCH=${CODEBUILD_GIT_BRANCH#remotes/origin/}
-echo "${CODEBUILD_GIT_BRANCH}"
-buildCommand="/work/cli build"
-if [ "${CODEBUILD_GIT_BRANCH}" == "main" ]; then
-    buildCommand="${buildCommand} --publish --send-notifications"
-fi
-
-echo "Running build command: ${buildCommand}"
-
-
 python --version
 pip install poetry
 poetry install --no-root
