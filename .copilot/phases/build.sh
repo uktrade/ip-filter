b09106351d2657c75218828ced8bcf9e83030e55
Samuele Mattiuzzo <1381563+samuele-mattiuzzo@users.noreply.github.com>
2024-05-31 14:29:50 +0100

    test

--- .copilot/phases/build.sh
+++ .copilot/phases/build.sh
@@ -7,18 +7,21 @@ set -e
 CODEBUILD_GIT_BRANCH=`git branch -a --contains HEAD | sed -n 2p | awk '{ printf $1 }'`
 CODEBUILD_GIT_BRANCH=${CODEBUILD_GIT_BRANCH#remotes/origin/}
 
-#echo "$CODEBUILD_GIT_BRANCH"
-#pwd
-#ls -al
-#cat .copilot/config.yml
-#cat .git
-#echo
+echo ">>>>> BUILD PHASE DEBUG <<<<<"
+echo "$CODEBUILD_GIT_BRANCH"
+pwd
+ls -al
+ls -al /codebuild/local-cache/workspace/ad88d84512be7140d005045a32fe3dc291db492c9644c95fe897bc60e5e683dd/
+cat .copilot/config.yml
+echo
+cat .git
+echo
 #export GIT_DIR=.
 #export GIT_WORK_TREE=.
 #rm -rf /codebuild/local-cache/workspace/ad88d84512be7140d005045a32fe3dc291db492c9644c95fe897bc60e5e683dd/
 #ECR_REPOSITORY=
 #export ADDITIONAL_ECR_REPOSITORY=public.ecr.aws/uktrade/ip-filter
-
+echo ">>>>> END BUILD PHASE DEBUG <<<<<"
 
 buildCommand="/work/cli build"
 if [ "${CODEBUILD_GIT_BRANCH}" == "main" ]; then
