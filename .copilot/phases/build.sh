afda340d94ba4c36359891b006e4835c9c39089c
Samuele Mattiuzzo <1381563+samuele-mattiuzzo@users.noreply.github.com>
2024-05-31 12:41:39 +0100

    test

--- .copilot/phases/build.sh
+++ .copilot/phases/build.sh
@@ -17,7 +17,7 @@ CODEBUILD_GIT_BRANCH=${CODEBUILD_GIT_BRANCH#remotes/origin/}
 #export GIT_WORK_TREE=.
 #rm -rf /codebuild/local-cache/workspace/ad88d84512be7140d005045a32fe3dc291db492c9644c95fe897bc60e5e683dd/
 #ECR_REPOSITORY=
-ADDITIONAL_ECR_REPOSITORY="public.ecr.aws/uktrade/ip-filter"
+export ADDITIONAL_ECR_REPOSITORY="public.ecr.aws/uktrade/ip-filter"
 
 buildCommand="/work/cli build"
 if [ "${CODEBUILD_GIT_BRANCH}" == "main" ]; then
